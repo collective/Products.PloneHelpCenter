96f946d1ac79b584efd1ccd0fb01ad4317e95df9
smcmahon <smcmahon@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-04-21 20:28:36 +0000

    eggification of PHC

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+version = '1.5beta6'
+
+setup(name='Products.PloneHelpCenter',
+      version=version,
+      description="A simple help-desk style documentation product for Plone.",
+      long_description=open("Products/PloneHelpCenter/README.txt").read() + \
+                        open("docs/INSTALL.txt").read() + \
+                        open("Products/PloneHelpCenter//HISTORY.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+      ],
+      keywords='Zope CMF Plone help documentation',
+      author='Plone Documentation Team',
+      author_email='plone-docs@lists.sourceforge.net',
+      url='http://svn.plone.org/svn/collective/Products.PloneHelpCenter/trunk',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      download_url='http://plone.org/products/plonehelpcenter',
+      install_requires=[
+        'setuptools',
+        'plone.i18n',
+      ],
+)
