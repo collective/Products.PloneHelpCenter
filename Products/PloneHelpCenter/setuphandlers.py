e85c17d44aab552922a9395ede57f2f4c53db1f5
smcmahon <smcmahon@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-04-25 20:43:58 +0000

    Use GS setup handler for extra steps

--- Products/PloneHelpCenter/setuphandlers.py
+++ Products/PloneHelpCenter/setuphandlers.py
@@ -1,25 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+setuphandlers.py
+
+Created by Steve McMahon on 2009-04-25.
+"""
+
 from StringIO import StringIO
+
 from Products.CMFCore.utils import getToolByName
 
 from Products.PloneHelpCenter import config
 
-EXTENSION_PROFILE = 'profile-Products.PloneHelpCenter:default'
-
-## how about the form controller settings?
 
 def install(self):
     out = StringIO()
 
-    tool=getToolByName(self, "portal_setup")
-
-    try:
-        tool.runAllImportStepsFromProfile(EXTENSION_PROFILE,
-            purge_old=False)
-    except AttributeError:   # before plone 3
-        old_context = tool.getImportContextID()
-        tool.setImportContext(EXTENSION_PROFILE)
-        tool.runAllImportSteps(purge_old=False)
-        tool.setImportContext(old_context)
+    # tool=getToolByName(self, "portal_setup")
+    # 
+    # try:
+    #     tool.runAllImportStepsFromProfile(EXTENSION_PROFILE,
+    #         purge_old=False)
+    # except AttributeError:   # before plone 3
+    #     old_context = tool.getImportContextID()
+    #     tool.setImportContext(EXTENSION_PROFILE)
+    #     tool.runAllImportSteps(purge_old=False)
+    #     tool.setImportContext(old_context)
 
     # Add catalog metadata columns and indexes
     catalog = getToolByName(self, 'portal_catalog')
@@ -108,33 +114,14 @@ def turnOnVersioning(site):
         for policy_id in DEFAULT_POLICIES:
             portal_repository.addPolicyForContentType(ctype, policy_id)    
 
-###########    
-# reinstalling by uninstalling and installing is way too common
-# for us to safely delete catalog data
-# 
-# def uninstall(self, reinstall=False):
-#     out = StringIO()
-# 
-#     if not reinstall:
-#         # Remove catalog metadata columns and indexes
-#         catalog = getToolByName(self, 'portal_catalog')
-#         removeCatalogIndex(self, out, catalog, 'isOutdated')
-#         removeCatalogMetadata(self, out, catalog, 'isOutdated')
-#         
-#         removeCatalogIndex(self, out, catalog, 'getAudiences')
-#         removeCatalogMetadata(self, out, catalog, 'getAudiences')
-#         
-#         removeCatalogIndex(self, out, catalog, 'getSections')
-#         removeCatalogMetadata(self, out, catalog, 'getSections')
-#         
-#         removeCatalogIndex(self, out, catalog, 'getStartHere')
-#         removeCatalogMetadata(self, out, catalog, 'getStartHere')
-#         
-#         removeCatalogIndex(self, out, catalog, 'isOutdated')
-#         removeCatalogMetadata(self, out, catalog, 'isOutdated')
-#         
-#         print >> out, "Removed PHC items from catalog indexes and metadata"
-# 
-# 
-#     print >> out, "Successfully uninstalled %s." % PROJECTNAME
-#     return out.getvalue()
+
+def importVarious(context):
+    """
+    Final plonehelpcenter import steps.
+    """
+    # Only run step if a flag file is present (e.g. not an extension profile)
+    if context.readDataFile('plonehelpcenter-various.txt') is None:
+        return
+    site = context.getSite()
+    print install(site)
+    
