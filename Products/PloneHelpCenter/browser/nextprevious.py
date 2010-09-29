623ec1aea82dffdc66cf859aa21e3b325569fd8e
davisagli <davisagli@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-09-29 03:58:02 +0000

    fix prev/next navigation

--- Products/PloneHelpCenter/browser/nextprevious.py
+++ Products/PloneHelpCenter/browser/nextprevious.py
@@ -3,14 +3,7 @@ from zope.component import adapts
 
 from plone.app.layout.nextprevious.interfaces import INextPreviousProvider
 
-from plone.memoize.instance import memoize
-
-from Acquisition import aq_parent, aq_inner
-
-from Products.CMFCore.utils import getToolByName
-
-from Products.PloneHelpCenter.interfaces import IHelpCenterMultiPage, \
-    IHelpCenterNavRoot
+from Products.PloneHelpCenter.interfaces import IHelpCenterMultiPage
 from Products.ATContentTypes.browser.nextprevious import ATFolderNextPrevious
 
 class HelpCenterFolderNextPrevious(ATFolderNextPrevious):
@@ -18,56 +11,28 @@ class HelpCenterFolderNextPrevious(ATFolderNextPrevious):
     will be automatically found by the @@plone_nextprevious_view and
     viewlet. 
 
-    Work recursively, so the next item of the last page of a given
-    section will be the next section, and the previous item of the
-    first page will be the previous section.
+    This is only used within types (ReferenceManual and Tutorial) that
+    provide a getTOCSelectOptions method which lists the pages in order,
+    so we use that.
     """
     
     implements(INextPreviousProvider)
     adapts(IHelpCenterMultiPage)
 
     def getNextItem(self, obj):
-        next = super(HelpCenterFolderNextPrevious, self).getNextItem(obj)
-
-        if next is None and not IHelpCenterNavRoot.providedBy(self.context):
-            # no next item in this section and we're not at a root element
-            parent = aq_parent(aq_inner(self.context))
-            return INextPreviousProvider(parent).getNextItem(self.context)
-        else: # normal behaviour
-            return next
-        
+        toc = obj.getTOCSelectOptions(current=obj)
+        use_next = False
+        for item in toc:
+            if use_next:
+                return item
+            if item['current']:
+                use_next = True
+    
     def getPreviousItem(self, obj):
-        previous = super(HelpCenterFolderNextPrevious, self).getPreviousItem(obj)
-        if previous is not None: 
-            return previous
-        else: # no previous item in this folder
-            parent = aq_parent(aq_inner(self.context))
-            return INextPreviousProvider(parent).getPreviousItem(self.context)
-
-
-    def buildNextPreviousQuery(self, position, range, sort_order = None):
-        sort_on                  = 'getObjPositionInParent'
-
-        query                    = {}
-        query['sort_on']         = sort_on
-        query['sort_limit']      = 1
-        query['path']            = dict(query = '/'.join(self.context.getPhysicalPath()),
-                                        depth = 1)
-
-        # Query the position using a range
-        if position == 0:
-            query[sort_on]       = 0
-        else:
-            query[sort_on]       = dict(query = position, range = range)
-
-        # Filters on content
-        query['is_default_page'] = False
-        query['object_provides'] = IHelpCenterMultiPage.__identifier__
-        # note we've deleted here the ATCT original:
-        # query['is_folderish'] = False
-
-        # Should I sort in any special way ?
-        if sort_order:
-            query['sort_order']  = sort_order
-
-        return query
+        toc = reversed(obj.getTOCSelectOptions(current=obj))
+        use_next = False
+        for item in toc:
+            if use_next:
+                return item
+            if item['current']:
+                use_next = True
